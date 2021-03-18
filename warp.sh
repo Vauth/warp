bdde7b0ba7a157947d7852cb7d0222f92dd044b9
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-03-18 23:10:47 +0800

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -98,7 +98,7 @@ systemctl enable wg-quick@wgcf
 echo 'precedence  ::ffff:0:0/96   100' | tee -a /etc/gai.conf
 
 # 删除临时目录和文件
-cd /root/ && rm -rf /root/warp/ /root/warp.sh
+cd /root/ && rm -rf /root/warp/ /root/warp*
 
 # 有wgcf的网络接口即为成功
 ip a
