d74e3e43fcf527ec512628a87051031754e72594
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-03-19 20:29:05 +0800

    Update warp6.sh

--- warp6.sh
+++ warp6.sh
@@ -59,7 +59,7 @@ fi
 
 # 以下为2个系统公共部分
 
-# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv6 的流量均被 WireGuard 接管，让 IPv6 的流量通过 WARP IPv4 节点以 NAT 的方式访问外部 IPv4 网络，为了防止当节点发生故障时 DNS 请求无法发出，修改为 IPv4 地址的 DNS
+# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv6 的流量均被 WireGuard 接管，让 IPv6 的流量通过 WARP IPv4 节点以 NAT 的方式访问外部 IPv6 网络，为了防止当节点发生故障时 DNS 请求无法发出，修改为 IPv4 地址的 DNS
 sudo sed -i '/0\.\0\/0/d' wgcf-profile.conf | sudo sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf | sudo sed -i 's/1.1.1.1/9.9.9.10,8.8.8.8,1.1.1.1/g' wgcf-profile.conf
 
 # 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
@@ -77,5 +77,5 @@ echo 'precedence  ::ffff:0:0/96   100' | sudo tee -a /etc/gai.conf
 # 删除临时目录和文件
 cd .. && rm -rf ./warp warp*
 
-# 有wgcf的网络接口即为成功
+# 有 wgcf 的网络接口即为成功
 ip a
