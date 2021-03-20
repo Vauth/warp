8fe4df427824e8d18f05362db3a22ce19291a521
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-03-20 16:16:35 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
-# 为 IPv4 only VPS 添加 WGCF
-
-# 创建安装暂时目录
-mkdir warp && cd warp
+##### 为 IPv4 only VPS 添加 WGCF #####
 
 # 判断系统，安装差异部分
 
@@ -20,11 +17,6 @@ if grep -q -E -i "ubuntu" /etc/issue; then
 	# 安装 wgcf
 	curl -fsSL git.io/wgcf.sh | sudo bash
 
-	# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
-	echo | sudo wgcf register
-
-	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
-	sudo wgcf generate
 
 # CentOS 运行以下脚本
      elif grep -q -E -i "kernel" /etc/issue; then
@@ -40,16 +32,11 @@ if grep -q -E -i "ubuntu" /etc/issue; then
 	sudo yum -y update
   
 	# 安装 wgcf
-	sudo wget -O wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
+	sudo wget -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
 
 	# 添加执行权限
-	sudo chmod +x /usr/bin/wireguard-go wgcf
+	sudo chmod +x /usr/bin/wireguard-go /usr/local/bin/wgcf
 
-	# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
-	echo | sudo ./wgcf register
-
-	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
-	sudo ./wgcf generate
 
 # 如都不符合，提示,删除临时文件并中止脚本
      else 
@@ -64,6 +51,12 @@ fi
 
 
 # 以下为2个系统公共部分
+
+# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
+echo | sudo wgcf register
+
+# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
+sudo wgcf generate
 
 # 修改配置文件 wgcf-profile.conf 的内容,使得 IPv6 的流量均被 WireGuard 接管，让 IPv6 的流量通过 WARP IPv4 节点以 NAT 的方式访问外部 IPv6 网络，为了防止当节点发生故障时 DNS 请求无法发出，修改为 IPv4 地址的 DNS
 sudo sed -i '/0\.\0\/0/d' wgcf-profile.conf | sudo sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf | sudo sed -i 's/1.1.1.1/9.9.9.10,8.8.8.8,1.1.1.1/g' wgcf-profile.conf
@@ -80,8 +73,8 @@ sudo systemctl enable wg-quick@wgcf
 # 优先使用 IPv4 网络
 echo 'precedence  ::ffff:0:0/96   100' | sudo tee -a /etc/gai.conf
 
-# 删除临时目录和文件
-cd .. && rm -rf ./warp warp*
+# 删除临时文件
+rm -f warp* wgcf*
 
 # 有 wgcf 的网络接口即为成功
 ip a
