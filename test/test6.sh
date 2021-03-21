99a959e1c450acb3abfe90c2c3332752637312e7
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-03-21 15:43:21 +0800

    Update test6.sh

--- test/test6.sh
+++ test/test6.sh
@@ -1,5 +1,5 @@
 ##### 为 IPv4 only VPS 添加 WGCF #####
-
+##### KVM 属于完整虚拟化的 VPS 主机，网络性能方面：内核模块＞wireguard-go。#####
 
 # 判断系统，安装差异部分
 
@@ -12,7 +12,7 @@ if grep -q -E -i "ubuntu" /etc/issue; then
 	# 安装一些必要的网络工具包和 wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
 	sudo apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
 	
-	# 安装 wireguard 内核模块 ( KVM 属于完整虚拟化的 VPS 主机，网络性能方面：内核模块＞wireguard-go)
+	# 安装 wireguard 内核模块
 	sudo apt -y --no-install-recommends install wireguard-dkms
 
 # CentOS 运行以下脚本
@@ -21,7 +21,7 @@ if grep -q -E -i "ubuntu" /etc/issue; then
 	# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
 	sudo yum -y install net-tools wireguard-tools
 
-	# 安装 wireguard 内核模块 ( KVM 属于完整虚拟化的 VPS 主机，网络性能方面：内核模块＞wireguard-go)
+	# 安装 wireguard 内核模块
 	sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 	sudo yum -y install epel-release wireguard-dkms
 
