1e5d98e453f15619a1316125d23b30d6b41f6acc
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-03-21 21:58:10 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -58,8 +58,7 @@ wget -N -P /usr/bin https://github.com/fscarmen/warp/raw/main/test/wireguard-go
 wget -N -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
 
 # 添加执行权限
-chmod +x /usr/local/bin/wgcf
-chmod +x /usr/bin/wireguard-go
+chmod +x /usr/local/bin/wgcf /usr/bin/wireguard-go
 
 # 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
 echo | wgcf register
