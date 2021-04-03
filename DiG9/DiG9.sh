85ca55088d9d5cce63ad2047e57f2deec56d72c5
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-04-03 14:25:49 +0800

    Update DiG9.sh

--- DiG9/DiG9.sh
+++ DiG9/DiG9.sh
@@ -1,8 +1,5 @@
 ##### 解决 Euserv 访问不了IPv4的问题，已在 Ubuntu 20.04 、 Debian 10 和 CentOS 8 测试通过 #####
 
-# 还原 EUserv 初始的 resolv.conf
-echo -e "search blue.kundencontroller.de\noptions rotate\nnameserver 2a02:180:6:5::1c\nnameserver 2a02:180:6:5::4\nnameserver 2a02:180:6:5::1e\nnameserver 2a02:180:6:5::1d" > /etc/resolv.conf
- 
 # 判断系统
 
 # Debian 运行以下脚本
@@ -38,7 +35,7 @@ if grep -q -E -i "debian" /etc/issue; then
 	
 
 	# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
-	yum -y install net-tools wireguard-tools
+	yum -6 -y install net-tools wireguard-tools
 
 
 # 如都不符合，提示,删除临时文件并中止脚本
