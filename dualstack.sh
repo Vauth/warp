ac057484011b0132adb751fe6fa3b3d1f9c887ca
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-04-22 21:38:21 +0800

    Update dualstack.sh

--- dualstack.sh
+++ dualstack.sh
@@ -49,10 +49,10 @@ fi
 # 以下为3类系统公共部分
 
 # 安装 wireguard-go
-wget -N -P /usr/bin https://github.com/fscarmen/warp/raw/main/wireguard-go
+wget -N -6 -P /usr/bin https://github.com/fscarmen/warp/raw/main/wireguard-go
 
 # 安装 wgcf
-wget -N -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
+wget -N -6 -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
 
 # 添加执行权限
 chmod +x /usr/bin/wireguard-go /usr/local/bin/wgcf
