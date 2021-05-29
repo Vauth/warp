f8b4609d23e671b8e165beed3a1b28df485c410d
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-05-29 14:50:34 +0800

    Create warparm.sh

---
+++ warparm.sh
@@ -0,0 +1,97 @@
+##### 为 ARM64 架构的 IPv6 only VPS 添加 WGCF，IPv6走 warp #####
+##### KVM 属于完整虚拟化的 VPS 主机，网络性能方面：内核模块＞wireguard-go。#####
+
+# 判断系统，安装差异部分
+
+# Debian 运行以下脚本
+if grep -q -E -i "debian" /etc/issue; then
+	
+	# 更新源
+	apt update
+
+	# 添加 backports 源,之后才能安装 wireguard-tools 
+	apt -y install lsb-release
+	echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
+
+	# 再次更新源
+	apt update
+
+	# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+	sudo apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools linux-headers-$(uname -r)
+	
+	# 安装 wireguard 内核模块
+	sudo apt -y --no-install-recommends install wireguard-dkms
+	
+# Ubuntu 运行以下脚本
+     elif grep -q -E -i "ubuntu" /etc/issue; then
+
+	# 更新源
+	sudo apt update
+
+	# 安装一些必要的网络工具包和 wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+	sudo apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
+
+# CentOS 运行以下脚本
+     elif grep -q -E -i "kernel" /etc/issue; then
+
+	# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+	sudo yum -y install epel-release
+	sudo yum -y install net-tools wireguard-tools
+
+	# 安装 wireguard 内核模块
+	sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+	sudo yum -y install epel-release wireguard-dkms
+
+	# 升级所有包同时也升级软件和系统内核
+	sudo yum -y update
+	
+	# 添加执行文件环境变量
+        export PATH=$PATH:/usr/local/bin
+
+# 如都不符合，提示,删除临时文件并中止脚本
+     else 
+	# 提示找不到相应操作系统
+	echo -e "\033[32m 抱歉，我不认识此系统！\033[0m"
+	
+	# 删除临时目录和文件，退出脚本
+	rm -f warp*
+	exit 0
+
+fi
+
+
+# 以下为3类系统公共部分
+
+# 安装 wgcf
+sudo wget -nc -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_arm64
+
+# 添加执行权限
+sudo chmod +x /usr/local/bin/wgcf
+
+# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
+echo | wgcf register
+
+# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
+wgcf generate
+
+# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv4 的流量均被 WireGuard 接管，让 IPv4 的流量通过 WARP IPv6 节点以 NAT 的方式访问外部 IPv4 网络
+sudo sed -i '/\:\:\/0/d' wgcf-profile.conf | sudo sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf
+
+# 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
+sudo cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+
+# 启用 Wire-Guard 网络接口守护进程
+sudo systemctl start wg-quick@wgcf
+
+# 设置开机启动
+sudo systemctl enable wg-quick@wgcf
+
+# 优先使用 IPv4 网络
+grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
+
+# 删除临时文件
+rm -f warp* wgcf*
+
+# 结果提示
+ip a | grep '.*wgcf:.*' "--color=auto"
+echo -e "\033[32m 结果：上一行是红字有 wgcf 的网络接口即为成功。如没有并报错 429 Too Many Requests ，可多次运行脚本直至成功。 \033[0m"
