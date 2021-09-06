4775468d5301d5989abfdd3a762c1221e2e900d4
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-06 23:50:56 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,4 +1,4 @@
-##### 为 IPv4 only VPS 添加 WGCF，双栈走 warp #####
+##### 为 IPv6 only VPS 添加 WGCF，IPv4走 warp #####
 ##### KVM 属于完整虚拟化的 VPS 主机，网络性能方面：内核模块＞wireguard-go。#####
 
 # 判断系统，安装差异部分
@@ -35,8 +35,7 @@ if grep -q -E -i "debian" /etc/issue; then
      elif grep -q -E -i "kernel" /etc/issue; then
 
 	# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
-	yum -y install sudo
-	sudo yum -y install epel-release
+	yum -y install epel-release sudo
 	sudo yum -y install net-tools wireguard-tools
 
 	# 安装 wireguard 内核模块
@@ -55,14 +54,14 @@ if grep -q -E -i "debian" /etc/issue; then
 	echo -e "\033[32m 抱歉，我不认识此系统！\033[0m"
 	
 	# 删除临时目录和文件，退出脚本
-	rm -f dualstack*
+	rm -f warp*
 	exit 0
 
 fi
 
 # 以下为3类系统公共部分
 
-# 判断系统架构是 AMD 还是 ARM，虚拟化是 LXC 还是 KVM,设置应用的依赖与环境
+# 判断系统架构是 AMD 还是 ARM
 if [[ $(hostnamectl) =~ .*arm.* ]]
         then architecture=arm64
         else architecture=amd64
@@ -72,7 +71,11 @@ fi
 latest=$(wget -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
 
 # 安装 wgcf
-sudo wget -N -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
+sudo wget -N -6 -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
+
+#### 安装 wireguard-go
+wget -N -6 -P /usr/bin wget -N https://cdn.jsdelivr.net/gh/fscarmen/warp/wireguard-go
+chmod +x /usr/bin/wireguard-go
 
 # 添加执行权限
 sudo chmod +x /usr/local/bin/wgcf
@@ -90,38 +93,31 @@ done
 # 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
 wgcf generate
 
-# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv6 的流量均被 WireGuard 接管，让 IPv6 的流量通过 WARP IPv4 节点以 NAT 的方式访问外部 IPv6 网络，为了防止当节点发生故障时 DNS 请求无法发出，修改为 IPv4 地址的 DNS
-sudo sed -i "7 s/^/PostUp = ip -4 rule add from $(ip a | egrep 'ens|eth0|enp' | awk -F '/' '{print $1}' | awk -F 'inet[ ]*' '{print $2}' | grep .) lookup main\n/" wgcf-profile.conf && sudo sed -i "8 s/^/PostDown = ip -4 rule delete from $(ip a | egrep 'ens|eth0|enp' | awk -F '/' '{print $1}' | awk -F 'inet[ ]*' '{print $2}' | grep .) lookup main\n/" wgcf-profile.conf && sudo sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf && sudo sed -i 's/1.1.1.1/9.9.9.10,8.8.8.8,1.1.1.1/g' wgcf-profile.conf
-
-# 如果系统原来是双栈，则需要添加 IPv6 的处理
-wget -qO- -6 ip.gs > /dev/null
-if [ $? -eq 0 ]; then
-	sed -i "8 s/^/PostUp = ip -6 rule add from $(wget -qO- -6 ip.gs) lookup main\n/" wgcf-profile.conf | sed -i "9 s/^/PostDown = ip -6 rule delete from $(wget -qO- -6 ip.gs) lookup main\n/" wgcf-profile.conf
-fi
+# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv4 的流量均被 WireGuard 接管，让 IPv4 的流量通过 WARP IPv6 节点以 NAT 的方式访问外部 IPv4 网络
+sudo sed -i '/\:\:\/0/d' wgcf-profile.conf | sudo sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf
 
 # 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
 sudo cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 
 # 删除临时文件
-rm -f dualstack* wgcf*
+rm -f warp* wgcf*
 
 # 自动刷直至成功（ warp bug，有时候获取不了ip地址）
 wg-quick up wgcf
-echo -e "\033[32m warp 获取 IP 中，如失败将自动重试直到成功。 \033[0m"
-wget -qO- -6 ip.gs > /dev/null
+wget -qO- -4 ip.gs > /dev/null
 until [ $? -eq 0 ]  
   do
+   echo -e "\033[32m warp 获取 IP 失败，自动重试直到成功。 \033[0m"
    wg-quick down wgcf
    wg-quick up wgcf
-   echo -e "\033[32m warp 获取 IP 失败，自动重试直到成功。 \033[0m"
-   wget -qO- -6 ip.gs > /dev/null
+   wget -qO- -4 ip.gs > /dev/null
 done
 
 # 设置开机启动
-systemctl enable wg-quick@wgcf > /dev/null
+sudo systemctl enable wg-quick@wgcf > /dev/null
 
 # 优先使用 IPv4 网络
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | tee -a /etc/gai.conf > /dev/null
 
 # 结果提示
-echo -e "\033[32m 恭喜！warp 双栈已成功，IPv4地址为:$(wget -qO- -4 ip.gs)，IPv6地址为:$(wget -qO- -6 ip.gs) \033[0m"
+echo -e "\033[32m 恭喜！为 IPv6 only VPS 添加 warp 已成功，IPv4地址为:$(wget -qO- -4 ip.gs) \033[0m"
