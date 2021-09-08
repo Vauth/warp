25ed72029063d1bcbd530d7290a7cfe9ebbdb026
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-08 17:22:01 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -106,14 +106,11 @@ rm -f dualstack* wgcf*
 
 # 自动刷直至成功（ warp bug，有时候获取不了ip地址）
 wg-quick up wgcf
-echo -e "\033[32m warp 获取 IP 中，如失败将自动重试直到成功。 \033[0m"
-wget -qO- -6 ip.gs > /dev/null
-until [ $? -eq 0 ]  
+until [ -n $(wget -qO- -6 ip.gs) ]
   do
    wg-quick down wgcf
    wg-quick up wgcf
-   echo -e "\033[32m warp 获取 IP 失败，自动重试直到成功。 \033[0m"
-   wget -qO- -6 ip.gs > /dev/null
+   echo -e "\033[32m warp 获取 IP 失败，自动重试直到成功。 \033[0m"	
 done
 
 # 设置开机启动
