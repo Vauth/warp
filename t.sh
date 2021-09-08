adce6a17102848ee65d8b539d50f35ee27402798
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-08 21:28:04 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,4 +1,4 @@
-##### 为 IPv6 only VPS 添加双栈 WGCF #####
+##### 为 IPv6 only VPS 添加 WGCF，双栈走 warp #####
 ##### LXC 非完整虚拟化 VPS 主机，选择 "wireguard-go" 方案。##### 
 
 # 判断系统，安装差异部分依赖包
@@ -40,7 +40,7 @@ if grep -q -E -i "debian" /etc/issue; then
 	echo -e "\033[32m 抱歉，我不认识此系统！\033[0m"
 	
 	# 删除临时目录和文件，退出脚本
-	rm -f warp.sh menu.sh
+	rm -f dualstack.sh menu.sh
 	exit 0
 
 fi
@@ -60,32 +60,31 @@ latest=$(wget -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/lat
 wget -N -6 -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
 
 # 安装 wireguard-go
-wget -N -6 -P /usr/bin wget -N https://cdn.jsdelivr.net/gh/fscarmen/warp/wireguard-go
+wget -N -6 -P /usr/bin https://cdn.jsdelivr.net/gh/fscarmen/warp/wireguard-go
 
 # 添加执行权限
 chmod +x /usr/bin/wireguard-go /usr/local/bin/wgcf
 
 # 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息，为避免文件已存在导致出错，先尝试删掉原文件)
-rm -f wgcf-account.toml
-echo | wgcf register
-until [ $? -eq 0 ]  
+rm -f wgcf-account.toml 
+until [[ -a wgcf-account.toml ]]
   do
+   echo | wgcf register
    echo -e "\033[32m warp 注册接口繁忙，5秒后自动重试直到成功。 \033[0m"
    sleep 5
-   echo | wgcf register
 done
 
 # 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
 wgcf generate
   
-# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv4 的流量均被 WireGuard 接管，让 IPv4 的流量通过 WARP IPv6 节点以 NAT 的方式访问外部 IPv4 网络
-sed -i '/\:\:\/0/d' wgcf-profile.conf | sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf
+# 修改配置文件 wgcf-profile.conf 的内容,使得 IPv4 IPv6 的流量均被 WireGuard 接管
+sed -i "7 s/^/PostUp = ip -6 rule add from $(ip route get 2400:3200::1 | grep -oP 'src \K\S+') lookup main\n/" wgcf-profile.conf && sed -i "8 s/^/PostDown = ip -6 rule delete from $(ip route get 2400:3200::1 | grep -oP 'src \K\S+') lookup main\n/" wgcf-profile.conf && sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf
 
 # 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 
 # 删除临时文件
-rm -f warp.sh wgcf-account.toml wgcf-profile.conf menu.sh
+rm -f dualstack.sh wgcf-account.toml wgcf-profile.conf menu.sh
 
 # 自动刷直至成功（ warp bug，有时候获取不了ip地址）
 wg-quick up wgcf
@@ -103,4 +102,4 @@ systemctl enable wg-quick@wgcf > /dev/null
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | tee -a /etc/gai.conf > /dev/null
 
 # 结果提示
-echo -e "\033[32m 恭喜！为 IPv6 only VPS 添加 warp 已成功，IPv4地址为:$(wget -qO- -4 ip.gs) \033[0m"
+echo -e "\033[32m 恭喜！warp 双栈已成功，IPv4地址为:$(wget -qO- -4 ip.gs)，IPv6地址为:$(wget -qO- -6 ip.gs) \033[0m"
