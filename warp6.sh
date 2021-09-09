97e2d624573edc9b9b6db0de554f371a4fc06d54
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-09 19:21:35 +0800

    Update warp6.sh

--- warp6.sh
+++ warp6.sh
@@ -100,12 +100,12 @@ sudo cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 rm -f warp6.sh wgcf-account.toml wgcf-profile.conf menu.sh
 
 # 自动刷直至成功（ warp bug，有时候获取不了ip地址）
+echo -e "\033[32m 后台获取 warp IP 中，有时候需10分钟，请耐心等待。 \033[0m"
 wg-quick up wgcf >/dev/null 2>&1
 until [[ -n $(wget -qO- -6 ip.gs) ]]
   do
    wg-quick down wgcf >/dev/null 2>&1
    wg-quick up wgcf >/dev/null 2>&1
-   echo -e "\033[32m warp 获取 IP 失败，自动重试直到成功。 \033[0m"	
 done
 
 # 设置开机启动
