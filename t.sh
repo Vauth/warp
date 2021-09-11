d909f93222ca0701f95df4f912e9ff63e1060466
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-12 01:37:11 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -47,128 +47,6 @@ if [[ $wgcf == WARP已开启 ]]
 	else plan=$virtualization$ipv4$ipv6
 fi
 
-# 判断系统，安装差异部分，安装依赖
-function install(){
-	green " (1/3) 安装系统依赖和 wireguard 内核模块 "
-
-	# 先删除之前安装，可能导致失败的文件
-	rm -f /usr/local/bin/wgcf /etc/wireguard/wgcf.conf /usr/bin/wireguard-go  wgcf-account.toml  wgcf-profile.conf
-	
-	# Debian 运行以下脚本
-	if [[ $(hostnamectl | tr A-Z a-z ) =~ debian ]]; then
-	
-		# 更新源
-		apt -y update
-
-		# 添加 backports 源,之后才能安装 wireguard-tools 
-		apt -y install lsb-release
-		echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
-
-		# 再次更新源
-		apt -y update
-
-		# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
-		apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
-
-		# 如 Linux 版本低于5.6并且是 kvm，则安装 wireguard 内核模块
-		if [[ $wg == 1 ]]; then apt -y --no-install-recommends install linux-headers-$(uname -r);apt -y --no-install-recommends install wireguard-dkms; fi
-
-	# Ubuntu 运行以下脚本
-	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ ubuntu ]]; then
-
-		# 更新源
-		apt -y update
-
-		# 安装一些必要的网络工具包和 wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
-		apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
-
-	# CentOS 运行以下脚本
-	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ centos ]]; then
-
-		# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
-		yum -y install epel-release
-		yum -y install net-tools wireguard-tools
-
-		# 如 Linux 版本低于5.6并且是 kvm，则安装 wireguard 内核模块
-		if [[ $wg == 1 ]]; then curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-		yum -y install epel-release wireguard-dkms; fi
-
-		# 升级所有包同时也升级软件和系统内核
-		yum -y update
-
-		# 添加执行文件环境变量
-		if [[ $PATH =~ /usr/local/bin ]]; then export PATH=$PATH; else export PATH=$PATH:/usr/local/bin; fi
-
-	# 如都不符合，提示,删除临时文件并中止脚本
-	     else 
-		# 提示找不到相应操作系统
-		green " 本脚本只支持 Debian、Ubuntu 和 CentOS 系统 "
-
-		# 删除临时目录和文件，退出脚本
-		rm -f menu.sh
-		exit 0
-	fi
-
-	# 安装并认证 WGCF
-	green " (2/3) 安装 WGCF "
-	# 判断系统架构是 AMD 还是 ARM
-	if [[ $(hostnamectl) =~ .*arm.* ]]; then architecture=arm64; else architecture=amd64; fi
-
-	# 判断 wgcf 的最新版本
-	latest=$(wget --no-check-certificate -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
-
-	# 安装 wgcf
-	wget -N --no-check-certificate -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
-
-	# 添加执行权限
-	chmod +x /usr/local/bin/wgcf
-	
-	# 如是 lXC，安装 wireguard-go
-	if [[ $virtualization == 1 ]]; then
- 	wget -N --no-check-certificate -P /usr/bin https://cdn.jsdelivr.net/gh/fscarmen/warp/wireguard-go
-	chmod +x /usr/bin/wireguard-go
-	fi
-	
-	# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息，为避免文件已存在导致出错，先尝试删掉原文件)
-	rm -f wgcf-account.toml
-	green " wgcf 注册中…… "
-	until [[ -a wgcf-account.toml ]]
-	  do
-	   echo | wgcf register >/dev/null 2>&1
-	done
-
-	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
-	wgcf generate >/dev/null 2>&1
-
-	# 修改配置文件
-	$(modify)
-
-	# 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
-	cp -f wgcf-profile.conf /etc/wireguard/wgcf.conf
-
-	# 自动刷直至成功（ warp bug，有时候获取不了ip地址）
-	green " (3/3) 运行 WGCF "
-	green " 后台获取 warp IP 中，有时候需10分钟，请耐心等待。"
-	wg-quick up wgcf >/dev/null 2>&1
-	until [[ -n $(wget -qO- -6 ip.gs) ]]
-	  do
-	   wg-quick down wgcf >/dev/null 2>&1
-	   wg-quick up wgcf >/dev/null 2>&1
-	done
-
-	# 设置开机启动
-	systemctl enable wg-quick@wgcf >/dev/null 2>&1
-
-	# 优先使用 IPv4 网络
-	if [[ -e /etc/gai.conf ]]; then grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | tee -a /etc/gai.conf >/dev/null 2>&1; fi
-
-	# 结果提示
-	green " 恭喜！WARP已开启，IPv4地址为:$(wget -qO- -4 ip.gs)，IPv6地址为:$(wget -qO- -6 ip.gs) "
-	
-	# 删除临时文件
-	rm -f menu.sh
-		}
-
 # VPS 当前状态
 function status(){
 	clear
@@ -324,6 +202,129 @@ function menu2(){
 			menu111;;
 		esac
 		}		
+
+# WGCF安装
+function install(){
+	green " (1/3) 安装系统依赖和 wireguard 内核模块 "
+
+	# 先删除之前安装，可能导致失败的文件
+	rm -f /usr/local/bin/wgcf /etc/wireguard/wgcf.conf /usr/bin/wireguard-go  wgcf-account.toml  wgcf-profile.conf
+	
+	# 判断系统，安装差异部分，安装依赖
+	# Debian 运行以下脚本
+	if [[ $(hostnamectl | tr A-Z a-z ) =~ debian ]]; then
+	
+		# 更新源
+		apt -y update
+
+		# 添加 backports 源,之后才能安装 wireguard-tools 
+		apt -y install lsb-release
+		echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
+
+		# 再次更新源
+		apt -y update
+
+		# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+		apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
+
+		# 如 Linux 版本低于5.6并且是 kvm，则安装 wireguard 内核模块
+		if [[ $wg == 1 ]]; then apt -y --no-install-recommends install linux-headers-$(uname -r);apt -y --no-install-recommends install wireguard-dkms; fi
+
+	# Ubuntu 运行以下脚本
+	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ ubuntu ]]; then
+
+		# 更新源
+		apt -y update
+
+		# 安装一些必要的网络工具包和 wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+		apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
+
+	# CentOS 运行以下脚本
+	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ centos ]]; then
+
+		# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
+		yum -y install epel-release
+		yum -y install net-tools wireguard-tools
+
+		# 如 Linux 版本低于5.6并且是 kvm，则安装 wireguard 内核模块
+		if [[ $wg == 1 ]]; then curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+		yum -y install epel-release wireguard-dkms; fi
+
+		# 升级所有包同时也升级软件和系统内核
+		yum -y update
+
+		# 添加执行文件环境变量
+		if [[ $PATH =~ /usr/local/bin ]]; then export PATH=$PATH; else export PATH=$PATH:/usr/local/bin; fi
+
+	# 如都不符合，提示,删除临时文件并中止脚本
+	     else 
+		# 提示找不到相应操作系统
+		green " 本脚本只支持 Debian、Ubuntu 和 CentOS 系统 "
+
+		# 删除临时目录和文件，退出脚本
+		rm -f menu.sh
+		exit 0
+	fi
+
+	# 安装并认证 WGCF
+	green " (2/3) 安装 WGCF "
+	# 判断系统架构是 AMD 还是 ARM
+	if [[ $(hostnamectl) =~ .*arm.* ]]; then architecture=arm64; else architecture=amd64; fi
+
+	# 判断 wgcf 的最新版本
+	latest=$(wget --no-check-certificate -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
+
+	# 安装 wgcf
+	wget -N --no-check-certificate -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
+
+	# 添加执行权限
+	chmod +x /usr/local/bin/wgcf
+	
+	# 如是 lXC，安装 wireguard-go
+	if [[ $virtualization == 1 ]]; then
+ 	wget -N --no-check-certificate -P /usr/bin https://cdn.jsdelivr.net/gh/fscarmen/warp/wireguard-go
+	chmod +x /usr/bin/wireguard-go
+	fi
+	
+	# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息，为避免文件已存在导致出错，先尝试删掉原文件)
+	rm -f wgcf-account.toml
+	green " wgcf 注册中…… "
+	until [[ -a wgcf-account.toml ]]
+	  do
+	   echo | wgcf register >/dev/null 2>&1
+	done
+
+	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
+	wgcf generate >/dev/null 2>&1
+
+	# 修改配置文件
+	$(modify)
+
+	# 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
+	cp -f wgcf-profile.conf /etc/wireguard/wgcf.conf
+
+	# 自动刷直至成功（ warp bug，有时候获取不了ip地址）
+	green " (3/3) 运行 WGCF "
+	green " 后台获取 warp IP 中，有时候需10分钟，请耐心等待。"
+	wg-quick up wgcf >/dev/null 2>&1
+	until [[ -n $(wget -qO- -6 ip.gs) ]]
+	  do
+	   wg-quick down wgcf >/dev/null 2>&1
+	   wg-quick up wgcf >/dev/null 2>&1
+	done
+
+	# 设置开机启动
+	systemctl enable wg-quick@wgcf >/dev/null 2>&1
+
+	# 优先使用 IPv4 网络
+	if [[ -e /etc/gai.conf ]]; then grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | tee -a /etc/gai.conf >/dev/null 2>&1; fi
+
+	# 结果提示
+	green " 恭喜！WARP已开启，IPv4地址为:$(wget -qO- -4 ip.gs)，IPv6地址为:$(wget -qO- -6 ip.gs) "
+	
+	# 删除临时文件
+	rm -f menu.sh
+		}
 
 case "$plan" in
    001 ) menu001;; 010 ) menu010;; 011 ) menu011;; 101 ) menu101;; 110 ) menu110;; 111 ) menu111;; 2 ) menu2;;
