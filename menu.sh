494bb0f205ef0531c867edddf49e42f007a25f0d
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-15 19:35:28 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -19,7 +19,7 @@ yellow(){
 [[ $(hostnamectl | grep -i Architecture) =~ arm ]] && architecture=arm64 || architecture=amd64
 
 # 判断虚拟化，选择 wireguard内核模块 还是 wireguard-go，	1=KVM,		2=openvz或者lxc
-[[ $(hostnamectl | grep -i Virtualization | awk -F ': ' '{print $2}') =~ openvz|lxc ]] && virtualization=1 || virtualization=0
+[[ $(hostnamectl | grep -i Virtualization) =~ openvz|lxc ]] && virtualization=1 || virtualization=0
 
 # 判断当前 IPv4 状态
 [[ $(ping -4 -c1 -W1 114.114.114.114) ]] >/dev/null 2>&1 && ipv4=1 || ipv4=0
