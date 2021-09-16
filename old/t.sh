de6465e1e29703873cffebbbb5c4dee54c056095
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-16 22:29:41 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -2,7 +2,7 @@
 grep -qE '^[ ]*\*/1[ ]*\*[ ]*\*[ ]*\*[ ]*\*[ ]*root[ ]*bash[ ]*/root/EU_docker_AutoUp.sh' /etc/crontab || echo '*/1 * * * *  root bash /root/EU_docker_AutoUp.sh' >> /etc/crontab
 
 # 生成 EU_docker_AutoUp.sh 文件，判断当前 docker 状态，遇到 Created 或 Exited 时重启，直至刷成功。1分钟后还没有刷成功，将不会重复该进程而浪费系统资源
-echo "[[ \$(pgrep -laf EU_docker_AutoUp | awk -F, '{a[\$2]++}END{for (i in a) print i"'" "'"a[i]}') -le 3 ]] && " >>EU_docker_AutoUp.sh
+echo "[[ \$(pgrep -laf ^[/d]*bash.*EU_docker_AutoUp | awk -F, '{a[\$2]++}END{for (i in a) print i"'" "'"a[i]}') -le 2 ]] && " >EU_docker_AutoUp.sh
 echo '	until [[ -z $(docker ps -a | egrep "Created|Exited") ]]' >>EU_docker_AutoUp.sh
 echo '		do docker start $(docker ps -a | egrep "Created|Exited" | awk '"'{print \$1}'); done" >>EU_docker_AutoUp.sh
 
