9d06e6f79efab565cc3bc2bc5220faf480467f36
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-18 16:06:32 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,6 +9,12 @@ yellow(){
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+# 判断操作系统，只支持 Debian、Ubuntu 或 Centos,如非上述操作系统，删除临时文件，退出脚本
+[[ $(hostnamectl | tr A-Z a-z) =~ debian ]] && system=debian
+[[ $(hostnamectl | tr A-Z a-z) =~ ubuntu ]] && system=ubuntu
+[[ $(hostnamectl | tr A-Z a-z) =~ centos ]] && system=centos
+[[ -z $system ]] && red " 本脚本只支持 Debian、Ubuntu 或 CentOS 系统 " && rm -f menu.sh && exit 0
+
 # 必须以root运行脚本
 [[ $(id -u) != 0 ]] && red " 必须以root方式运行脚本,可以输入 sudo -i 后重新下载运行。 " && exit 0
 
@@ -34,7 +40,7 @@ v6=$(wget -T1 -t1 -qO- -6 ip.gs) >/dev/null 2>&1
 [[ $warpv4 = 1 || $warpv6 = 1 ]] && plan=2 || plan=$ipv4$ipv6
 
 # 在KVM的前提下，判断 Linux 版本是否小于 5.6，如是则安装 wireguard 内核模块，变量 wg=1。由于 linux 不能直接用小数作比较，所以用 （主版本号 * 100 + 次版本号 ）与 506 作比较
-[[ $virtualization -eq 0 && $(($(uname  -r | awk -F . '{print $1 }') * 100 +  $(uname  -r | awk -F . '{print $2 }'))) -lt 506 ]] && wg=1
+[[ $virtualization = 0 && $(($(uname  -r | cut -d . -f1) * 100 +  $(uname  -r | cut -d . -f2))) -lt 506 ]] && wg=1
 
 # WGCF 配置修改
 modify1="sed -i '/\:\:\/0/d' wgcf-profile.conf && sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf"
@@ -44,29 +50,30 @@ modify4='sed -i "7 s/^/PostUp = ip -4 rule add from $(ip route get 114.114.114.1
 modify5='sed -i "7 s/^/PostUp = ip -4 rule add from $(ip route get 114.114.114.114 | grep -oP '"'src \K\S+') lookup main\n/"'" wgcf-profile.conf && sed -i "8 s/^/PostDown = ip -4 rule delete from $(ip route get 114.114.114.114 | grep -oP '"'src \K\S+') lookup main\n/"'" wgcf-profile.conf && sed -i "9 s/^/PostUp = ip -6 rule add from $(ip route get 2400:3200::1 | grep -oP '"'src \K\S+') lookup main\n/"'" wgcf-profile.conf && sed -i "10 s/^/PostDown = ip -6 rule delete from $(ip route get 2400:3200::1 | grep -oP '"'src \K\S+') lookup main\n/"'" wgcf-profile.conf && sed -i '"'s/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf && sed -i 's/1.1.1.1/9.9.9.9,8.8.8.8,1.1.1.1/g' wgcf-profile.conf"
 
 # VPS 当前状态
-function status(){
+status(){
 	clear
 	yellow "本项目专为 VPS 添加 wgcf 网络接口，详细说明：https://github.com/fscarmen/warp\n脚本特点:\n	* 根据不同系统综合情况显示不同的菜单，避免出错\n	* 结合 Linux 版本和虚拟化方式，自动优选三个 WireGuard 方案。网络性能方面：内核集成 WireGuard＞安装内核模块＞wireguard-go\n	* 智能判断 WGCF 作者 github库的最新版本 （Latest release\n	* 智能判断vps操作系统：Ubuntu 18.04、Ubuntu 20.04、Debian 10、Debian 11、CentOS 7、CentOS 8，请务必选择 LTS 系统\n	* 智能判断硬件结构类型：Architecture 为 AMD 或者 ARM\n	* 智能分析内网和公网IP生成 WGCF 配置文件\n	* 输出结果，提示是否使用 WARP IP ，并自动清理安装时的临时文件\n"
 	red "======================================================================================================================\n"
-	green " 系统信息：\n	当前操作系统：$(hostnamectl | grep -i operating | awk -F ':' '{print $2}')\n	内核：$(uname -r)\n	处理器架构：$architecture\n	虚拟化：$(hostnamectl | grep -i virtualization | awk -F ': ' '{print $2}') "
+	green " 系统信息：\n	当前操作系统：$(hostnamectl | grep -i operating | cut -d : -f2)\n	内核：$(uname -r)\n	处理器架构：$architecture\n	虚拟化：$(hostnamectl | grep -i virtualization | cut -d : -f2) "
 	[[ $warpv4 = 1 ]] && green "	IPv4：$v4 ( WARP IPv4 ) " || green "	IPv4：$v4 "
 	[[ $warpv6 = 1 ]] && green "	IPv6：$v6 ( WARP IPv6 ) " || green "	IPv6：$v6 "
 	[[ $plan = 2 ]] && green "	WARP 已开启" || green "	WARP 未开启 "
  	red "\n======================================================================================================================\n"
-		}    
+		}
 
 # WGCF 安装
-function install(){
+install(){
 	startTime_s=`date +%s`
 	green " 进度  1/3： 安装系统依赖 "
 
 	# 先删除之前安装，可能导致失败的文件
 	rm -f /usr/local/bin/wgcf /etc/wireguard/wgcf.conf /usr/bin/wireguard-go  wgcf-account.toml  wgcf-profile.conf
-
-	# 判断系统，安装差异部分，安装依赖
+	
+        # 根据系统选择需要安装的依赖
+        case "$system" in
+	
 	# Debian 运行以下脚本
-	if [[ $(hostnamectl | tr A-Z a-z ) =~ debian ]]; then
-
+	debian )
 		# 更新源
 		apt -y update
 
@@ -82,19 +89,19 @@ function install(){
 
 		# 如 Linux 版本低于5.6并且是 kvm，则安装 wireguard 内核模块
 		[[ $wg = 1 ]] && apt -y --no-install-recommends install linux-headers-$(uname -r) && apt -y --no-install-recommends install wireguard-dkms
-
+		;;
+		
 	# Ubuntu 运行以下脚本
-	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ ubuntu ]]; then
-
+	ubuntu )
 		# 更新源
 		apt -y update
 
 		# 安装一些必要的网络工具包和 wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
 		apt -y --no-install-recommends install net-tools iproute2 openresolv dnsutils wireguard-tools
-
+		;;
+		
 	# CentOS 运行以下脚本
-	     elif [[ $(hostnamectl | tr A-Z a-z ) =~ centos ]]; then
-
+	centos )
 		# 安装一些必要的网络工具包和wireguard-tools (Wire-Guard 配置工具：wg、wg-quick)
 		yum -y install epel-release
 		yum -y install curl net-tools wireguard-tools
@@ -108,22 +115,14 @@ function install(){
 
 		# 添加执行文件环境变量
 		[[ $PATH =~ /usr/local/bin ]] || export PATH=$PATH:/usr/local/bin
-
-	# 如都不符合，提示,删除临时文件并中止脚本
-	     else 
-		# 提示找不到相应操作系统
-		green " 本脚本只支持 Debian、Ubuntu 和 CentOS 系统 "
-
-		# 删除临时目录和文件，退出脚本
-		rm -f menu.sh
-		exit 0
-	fi
+		;;
+        esac
 
 	# 安装并认证 WGCF
 	green " 进度  2/3： 安装 WGCF "
 
 	# 判断 wgcf 的最新版本
-	latest=$(wget --no-check-certificate -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
+	latest=$(wget --no-check-certificate -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | cut -d : -f2 | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
 
 	# 安装 wgcf
 	wget -N --no-check-certificate -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
@@ -178,14 +177,14 @@ function install(){
 		}
 
 # 关闭 WARP 网络接口，并删除 WGCF
-function uninstall(){
-	systemctl disable wg-quick@$(wg | grep interface: |awk '{print $2}') 2>/dev/null
-	wg-quick down $(wg | grep interface: |awk '{print $2}') 2>/dev/null
+uninstall(){
+	systemctl disable wg-quick@$(wg | grep interface | cut -d : -f2) 2>/dev/null
+	wg-quick down $(wg | grep interface | cut -d : -f2) 2>/dev/null
 	apt -y autoremove wireguard-tools wireguard-dkms 2>/dev/null
 	yum -y autoremove wireguard-tools wireguard-dkms 2>/dev/null
 	rm -rf /usr/local/bin/wgcf /etc/wireguard /usr/bin/wireguard-go  wgcf-account.toml  wgcf-profile.conf menu.sh
 	[[ -e /etc/gai.conf ]] && sed -i '/^precedence[ ]*::ffff:0:0\/96[ ]*100/d' /etc/gai.conf
-	[[ -z $(wg) ]] >/dev/null 2>&1 && green " WGCF 已彻底删除 " || red " 没有清除干净，请重启(reboot)后尝试再次删除 "
+	[[ -z $(wg) ]] >/dev/null 2>&1 && green " WGCF 已彻底删除!\n IPv4：$(wget -T1 -t1 -qO- -4 ip.gs)\n IPv6：$(wget -T1 -t1 -qO- -6 ip.gs) " || red " 没有清除干净，请重启(reboot)后尝试再次删除 "
 		}
 
 # 安装BBR
@@ -204,7 +203,7 @@ bbrInstall() {
 		}
 
 # IPv6
-function menu01(){
+menu01(){
 	status
 	green " 1. 为 IPv6 only 添加 IPv4 网络接口 "
 	green " 2. 为 IPv6 only 添加双栈网络接口 "
@@ -223,7 +222,7 @@ function menu01(){
 		}
 
 # IPv4
-function menu10(){
+menu10(){
 	status
 	green " 1. 为 IPv4 only 添加 IPv6 网络接口 "
 	green " 2. 为 IPv4 only 添加双栈网络接口 "
@@ -242,7 +241,7 @@ function menu10(){
 		}
 
 # IPv4+IPv6
-function menu11(){ 
+menu11(){ 
 	status
 	green " 1. 为 原生双栈 添加 WARP双栈 网络接口 "
 	green " 2. 关闭 WARP 网络接口，并删除 WGCF "
@@ -259,7 +258,7 @@ function menu11(){
 		}
 
 # 已开启 warp 网络接口
-function menu2(){ 
+menu2(){ 
 	status
 	green " 1. 关闭 WARP 网络接口，并删除 WGCF "
 	green " 2. 升级内核、安装BBR、DD脚本 "
