5dce9871af854cf68953cc60804d8438499c5134
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-22 15:08:00 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -127,8 +127,9 @@ install(){
 	latest=$(wget --no-check-certificate -qO- -t1 -T2 "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | cut -d : -f2 | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
 	[[ -z $latest ]] && latest='2.2.8'
 
-	# 安装 wgcf
+	# 安装 wgcf，如官方 wgcf 下载不成功，将使用 jsDelivr 的 CDN，以更好的支持双栈
 	wget -N --no-check-certificate -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v$latest/wgcf_${latest}_linux_$architecture
+	[[  $? != 0 ]] && wget -N --no-check-certificate -O /usr/local/bin/wgcf https://cdn.jsdelivr.net/gh/fscarmen/warp/wgcf_2.2.8_linux_$architecture
 
 	# 添加执行权限
 	chmod +x /usr/local/bin/wgcf
