cddb6f2654e8a8f9bad840cdcdd7e0776d3b2e5b
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-03 04:54:37 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -155,7 +155,7 @@ install(){
 	
 	# 如有 Warp+ 账户，修改 license 并升级
 	[[ -n $LICENSE ]] && yellow " 升级 Warp+ 账户 " && sed -i "s/license_key.*/license_key = \"$LICENSE\"/g" wgcf-account.toml && wgcf update
-	[[ $? != 0 ]] && red " 升级失败，Warp+ 账户错误或者已激活超过5台设备 "
+	[[ $? != 0 ]] && red " 升级失败，Warp+ 账户错误或者已激活超过5台设备，换免费 Warp 账户继续 "
 	
 	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
 	wgcf generate >/dev/null 2>&1
