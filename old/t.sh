693364fae158f2ceff51b5e8cc3168a1cddf903c
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-05 09:37:48 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -9,6 +9,75 @@ yellow(){
 	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+# 关闭 WARP 网络接口，并删除 WGCF
+uninstall(){
+	unset WAN4 WAN6 COUNTRY4 COUNTRY6
+	systemctl disable wg-quick@$(wg | grep interface | cut -d : -f2) >/dev/null 2>&1
+	wg-quick down $(wg | grep interface | cut -d : -f2) >/dev/null 2>&1
+	[[ $SYSTEM = centos ]] && yum -y autoremove wireguard-tools wireguard-dkms 2>/dev/null || apt -y autoremove wireguard-tools wireguard-dkms 2>/dev/null
+	rm -rf /usr/local/bin/wgcf /etc/wireguard /usr/bin/wireguard-go /etc/wireguard wgcf-account.toml wgcf-profile.conf menu.sh
+	[[ -e /etc/gai.conf ]] && sed -i '/^precedence[ ]*::ffff:0:0\/96[ ]*100/d' /etc/gai.conf
+	sed -i '/^@reboot.*WARP_AutoUp/d' /etc/crontab
+	WAN4=$(wget --no-check-certificate -T1 -t1 -qO- -4 ip.gs)
+	WAN6=$(wget --no-check-certificate -T1 -t1 -qO- -6 ip.gs)
+	COUNTRY4=$(wget --no-check-certificate -T1 -t1 -qO- -4 https://ip.gs/country)
+	COUNTRY6=$(wget --no-check-certificate -T1 -t1 -qO- -6 https://ip.gs/country)
+	[[ -z $(wg) ]] >/dev/null 2>&1 && green " WGCF 已彻底删除!\n IPv4：$WAN4 $COUNTRY4\n IPv6：$WAN6 $COUNTRY6 " || red " 没有清除干净，请重启(reboot)后尝试再次删除 "
+		}
+
+# 安装BBR
+bbrInstall() {
+	red "\n=============================================================="
+	green "BBR、DD脚本用的[ylx2016]的成熟作品，地址[https://github.com/ylx2016/Linux-NetSpeed]，请熟知"
+	yellow "1.安装脚本【推荐原版BBR+FQ】"
+	yellow "2.回退主目录"
+	red "=============================================================="
+	read -p "请选择：" BBR
+	case "$BBR" in
+		1 ) wget --no-check-certificate -N "https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh;;
+		2 ) menu$PLAN;;
+		* ) red "请输入正确数字 [1-2]"; sleep 1; bbrInstall;;
+		esac
+		}
+
+input() {
+	read -p " 请输入 Warp+ ID: " ID
+	i=5
+	until [[ ${#ID} = 36 || $i = 1 ]]
+		do
+		let i--
+		red " Warp+ ID 应为36位数 "
+		read -p " 请重新输入 Warp+ ID （剩余$i次）:" ID
+	done
+	[[ $i = 1 ]] && red " 输入错误达5次，脚本退出 " && exit 0
+	}
+
+# 刷 Warp+ 流量
+plus() {
+	red "\n=============================================================="
+	green " 刷 Warp+ 流量用可选择以下两位作者的成熟作品，请熟知:\n	* [ALIILAPRO]，地址[https://github.com/ALIILAPRO/warp-plus-cloudflare]\n	* [mixool]，地址[https://github.com/mixool/across/tree/master/wireguard]\n 下载地址：https://1.1.1.1/，访问和苹果外区 ID 自理\n 获取 Warp+ ID 填到下面。方法：App右上角菜单 三 --> 高级 --> 诊断 --> ID\n 重要：刷脚本后流量没有增加处理：右上角菜单 三 --> 高级 --> 连接选项 --> 重置加密密钥\n 最好配合 screen 在后台运行任务 "
+	yellow "1.运行[ALIILAPRO]脚本 "
+	yellow "2.运行[mixool]脚本 "
+	yellow "3.回退主目录"
+	red "=============================================================="
+	read -p "请选择：" CHOOSEPLUS
+	case "$CHOOSEPLUS" in
+		1 ) input
+		    [[ $(type -P git) ]] || apt -y install git 2>/dev/null || yum -y install git 2>/dev/null
+		    [[ $(type -P python3) ]] || apt -y install python3 2>/dev/null || yum -y install python3 2>/dev/null
+		    [[ -d ~/warp-plus-cloudflare ]] || git clone https://github.com/aliilapro/warp-plus-cloudflare.git
+		    echo $ID | python3 ~/warp-plus-cloudflare/wp-plus.py;;
+		2 ) input
+		    read -p " 你希望获取的目标流量值，单位为 GB，输入数字即可，默认值为10 :" MISSION
+		    wget --no-check-certificate -N https://cdn.jsdelivr.net/gh/mixool/across/wireguard/warp_plus.sh
+		    sed -i "s/eb86bd52-fe28-4f03-a944-60428823540e/$ID/g" warp_plus.sh
+		    bash warp_plus.sh $(echo $MISSION | sed 's/[^0-9]*//g');;
+		3 ) menu$PLAN;;
+		* ) red "请输入正确数字 [1-3]"; sleep 1; plus;;
+		esac
+		}
+
+main(){
 # 判断是否大陆 VPS，如连不通 CloudFlare 的 IP，则 WARP 项目不可用
 ping -4 -c1 -W1 162.159.192.1 >/dev/null 2>&1 && IPV4=1 || IPV4=0
 ping -6 -c1 -W1 2606:4700:d0::a29f:c001 >/dev/null 2>&1 && IPV6=1 || IPV6=0
@@ -223,75 +292,6 @@ install(){
 	[[ $TRACE4 = plus || $TRACE6 = plus ]] && green " 恭喜！WARP+ 已开启，总耗时:$(( $end - $start ))秒 "
 	[[ $TRACE4 = on || $TRACE6 = on ]] && green " 恭喜！WARP 已开启，总耗时:$(( $end - $start ))秒 " 	
 	[[ $TRACE4 = off && $TRACE6 = off ]] && red " WARP 安装失败，问题反馈:[https://github.com/fscarmen/warp/issues] "
-		}
-
-# 关闭 WARP 网络接口，并删除 WGCF
-uninstall(){
-	unset WAN4 WAN6 COUNTRY4 COUNTRY6
-	systemctl disable wg-quick@$(wg | grep interface | cut -d : -f2) >/dev/null 2>&1
-	wg-quick down $(wg | grep interface | cut -d : -f2) >/dev/null 2>&1
-	[[ $SYSTEM = centos ]] && yum -y autoremove wireguard-tools wireguard-dkms 2>/dev/null || apt -y autoremove wireguard-tools wireguard-dkms 2>/dev/null
-	rm -rf /usr/local/bin/wgcf /etc/wireguard /usr/bin/wireguard-go /etc/wireguard wgcf-account.toml wgcf-profile.conf menu.sh
-	[[ -e /etc/gai.conf ]] && sed -i '/^precedence[ ]*::ffff:0:0\/96[ ]*100/d' /etc/gai.conf
-	sed -i '/^@reboot.*WARP_AutoUp/d' /etc/crontab
-	WAN4=$(wget --no-check-certificate -T1 -t1 -qO- -4 ip.gs)
-	WAN6=$(wget --no-check-certificate -T1 -t1 -qO- -6 ip.gs)
-	COUNTRY4=$(wget --no-check-certificate -T1 -t1 -qO- -4 https://ip.gs/country)
-	COUNTRY6=$(wget --no-check-certificate -T1 -t1 -qO- -6 https://ip.gs/country)
-	[[ -z $(wg) ]] >/dev/null 2>&1 && green " WGCF 已彻底删除!\n IPv4：$WAN4 $COUNTRY4\n IPv6：$WAN6 $COUNTRY6 " || red " 没有清除干净，请重启(reboot)后尝试再次删除 "
-		}
-
-# 安装BBR
-bbrInstall() {
-	red "\n=============================================================="
-	green "BBR、DD脚本用的[ylx2016]的成熟作品，地址[https://github.com/ylx2016/Linux-NetSpeed]，请熟知"
-	yellow "1.安装脚本【推荐原版BBR+FQ】"
-	yellow "2.回退主目录"
-	red "=============================================================="
-	read -p "请选择：" BBR
-	case "$BBR" in
-		1 ) wget --no-check-certificate -N "https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh;;
-		2 ) menu$PLAN;;
-		* ) red "请输入正确数字 [1-2]"; sleep 1; bbrInstall;;
-		esac
-		}
-
-input() {
-	read -p " 请输入 Warp+ ID: " ID
-	i=5
-	until [[ ${#ID} = 36 || $i = 1 ]]
-		do
-		let i--
-		red " Warp+ ID 应为36位数 "
-		read -p " 请重新输入 Warp+ ID （剩余$i次）:" ID
-	done
-	[[ $i = 1 ]] && red " 输入错误达5次，脚本退出 " && exit 0
-	}
-
-# 刷 Warp+ 流量
-plus() {
-	red "\n=============================================================="
-	green " 刷 Warp+ 流量用可选择以下两位作者的成熟作品，请熟知:\n	* [ALIILAPRO]，地址[https://github.com/ALIILAPRO/warp-plus-cloudflare]\n	* [mixool]，地址[https://github.com/mixool/across/tree/master/wireguard]\n 下载地址：https://1.1.1.1/，访问和苹果外区 ID 自理\n 获取 Warp+ ID 填到下面。方法：App右上角菜单 三 --> 高级 --> 诊断 --> ID\n 重要：刷脚本后流量没有增加处理：右上角菜单 三 --> 高级 --> 连接选项 --> 重置加密密钥\n 最好配合 screen 在后台运行任务 "
-	yellow "1.运行[ALIILAPRO]脚本 "
-	yellow "2.运行[mixool]脚本 "
-	yellow "3.回退主目录"
-	red "=============================================================="
-	read -p "请选择：" CHOOSEPLUS
-	case "$CHOOSEPLUS" in
-		1 ) input
-		    [[ $(type -P git) ]] || apt -y install git 2>/dev/null || yum -y install git 2>/dev/null
-		    [[ $(type -P python3) ]] || apt -y install python3 2>/dev/null || yum -y install python3 2>/dev/null
-		    [[ -d ~/warp-plus-cloudflare ]] || git clone https://github.com/aliilapro/warp-plus-cloudflare.git
-		    echo $ID | python3 ~/warp-plus-cloudflare/wp-plus.py;;
-		2 ) input
-		    read -p " 你希望获取的目标流量值，单位为 GB，输入数字即可，默认值为10 :" MISSION
-		    wget --no-check-certificate -N https://cdn.jsdelivr.net/gh/mixool/across/wireguard/warp_plus.sh
-		    sed -i "s/eb86bd52-fe28-4f03-a944-60428823540e/$ID/g" warp_plus.sh
-		    bash warp_plus.sh $(echo $MISSION | sed 's/[^0-9]*//g');;
-		3 ) menu$PLAN;;
-		* ) red "请输入正确数字 [1-3]"; sleep 1; plus;;
-		esac
-		}
 
 # IPv6
 menu01(){
@@ -370,12 +370,14 @@ menu2(){
 		* ) 	red "请输入正确数字 [0-3]"; sleep 1; menu2;;
 		esac
 		}
+		}
+menu$PLAN
+}
 
-# menu$PLAN
 
 ACTION=$1
 case "$ACTION" in
 bbr )	bbrInstall;;
 warp+ )	plus;;
-* )	menu$PLAN
+* )	main
 esac
