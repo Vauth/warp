838e01aeb317de7f0164a0824331fce3cc01c329
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-09 21:46:35 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -335,7 +335,7 @@ update() {
 
 # 同步脚本至最新版本
 ver(){
-	wget -N $CDN -P /etc/wireguard/ https://cdn.jsdelivr.net/gh/fscarmen/warp/menu.sh &&
+	wget -N $CDN -P /etc/wireguard https://cdn.jsdelivr.net/gh/fscarmen/warp/menu.sh &&
 	chmod +700 menu.sh &&
 	cp -f menu.sh /usr/bin/warp &&
 	green " 成功！已同步最新脚本，版本号：$VERSION " || red " 升级失败，问题反馈:[https://github.com/fscarmen/warp/issues] "
