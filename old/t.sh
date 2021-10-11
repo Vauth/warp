af0265fcbd629aefd65821f267adde666b9d241d
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-11 11:01:25 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -71,15 +71,19 @@ net(){
 	[[ $LXC = 1 ]] && UP='WG_QUICK_USERSPACE_IMPLEMENTATION=boringtun WG_SUDO=1 wg-quick up wgcf' || UP='wg-quick up wgcf'
 	[[ ! $(type -P wg-quick) || ! -e /etc/wireguard/wgcf.conf ]] && red " 没有安装 WireGuard tools 或者找不到配置文件 wgcf.conf，请重新安装 " && exit ||
 	yellow " 后台获取 WARP IP 中……  "
+	i=1
 	echo $UP | sh >/dev/null 2>&1
-	WAN4=$(curl -s4m3 https://ip.gs)
-	WAN6=$(curl -s6m3 https://ip.gs)
-	until [[ -n $WAN4 && -n $WAN6 ]]
-		do	wg-quick down wgcf >/dev/null 2>&1
+	WAN4=$(curl -s4m4 https://ip.gs)
+	WAN6=$(curl -s6m4 https://ip.gs)
+	until [[ -n $WAN4 && -n $WAN6 || $i = 20 ]]
+		do	yellow " 第$i次尝试 "
+			let i++
+			wg-quick down wgcf >/dev/null 2>&1
 			echo $UP | sh >/dev/null 2>&1
-			WAN4=$(curl -s4m3 https://ip.gs)
-			WAN6=$(curl -s6m3 https://ip.gs)
-	done
+			WAN4=$(curl -s4m4 https://ip.gs)
+			WAN6=$(curl -s6m4 https://ip.gs)
+		done
+	[[ $i = 20 ]] && red " 失败已超过$i次，脚本中止，问题反馈:[https://github.com/fscarmen/warp/issues] " && exit
 	}
 
 # WARP 开关
