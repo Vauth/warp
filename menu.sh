f515d38a2c07b98da968d5249b22320e07d8bc5f
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-23 10:39:39 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -222,7 +222,7 @@ install(){
 	# 如有 Warp+ 账户，修改 license 并升级，并把设备名等信息保存到 /etc/wireguard/info.log
 	mkdir -p /etc/wireguard/ >/dev/null 2>&1
 	[[ -n $LICENSE ]] && yellow " 升级 Warp+ 账户 " && sed -i "s/license_key.*/license_key = \"$LICENSE\"/g" wgcf-account.toml &&
-	( wgcf update | tee /etc/wireguard/info.log || red " 升级失败，Warp+ 账户错误或者已激活超过5台设备，自动更换免费 Warp 账户继续 " )
+	( wgcf update > /etc/wireguard/info.log 2>&1 || red " 升级失败，Warp+ 账户错误或者已激活超过5台设备，自动更换免费 Warp 账户继续 " )
 	
 	# 生成 Wire-Guard 配置文件 (wgcf-profile.conf)
 	wgcf generate >/dev/null 2>&1
@@ -353,7 +353,7 @@ update() {
         done
 	cd /etc/wireguard
 	sed -i "s#license_key.*#license_key = \"$LICENSE\"#g" wgcf-account.toml &&
-	wgcf update | tee /etc/wireguard/info.log &&
+	wgcf update > /etc/wireguard/info.log 2>&1 &&
 	(wgcf generate >/dev/null 2>&1
 	sed -i "2s#.*#$(sed -ne 2p wgcf-profile.conf)#" wgcf.conf
 	sed -i "3s#.*#$(sed -ne 3p wgcf-profile.conf)#" wgcf.conf
