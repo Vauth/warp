7147954dd313e8731cfb14031c52b69391c51a9c
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-06 23:59:38 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -562,11 +562,10 @@ proxy(){
 
 	# 设置为代理模式
 	green " $T84 "
-	warp-cli --accept-tos register
-	warp-cli --accept-tos set-mode proxy
-	warp-cli --accept-tos connect
-	warp-cli --accept-tos enable-always-on
-	sleep 2
+	warp-cli --accept-tos register >/dev/null 2>&1 &&
+	warp-cli --accept-tos set-mode proxy >/dev/null 2>&1 &&
+	warp-cli --accept-tos connect >/dev/null 2>&1 &&
+	warp-cli --accept-tos enable-always-on >/dev/null 2>&1 &&
 	[[ ! $(ss -nltp) =~ '127.0.0.1:40000' ]] && red " $T87 " && exit 1 || green " $T86 "
 
 	# 创建再次执行的软链接快捷方式，再次运行可以用 warp 指令
