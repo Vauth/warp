461ab7c6112a5d8882fc6fa0a9a631ccfaa951d4
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-12 01:12:04 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -429,7 +429,7 @@ stack_priority(){
 		3 )	;;
 		* )	echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf;;
 	esac
-[[ $(curl -sm8 https://ip.gs) =~ : ]] && green " T106 " || green " T107 "
+[[ $(curl -sm8 https://ip.gs) =~ : ]] && green " $T106 " || green " $T107 "
 }
 
 # WGCF 安装
