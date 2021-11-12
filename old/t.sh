4086eee7169c68e0817815ae10db4b43e172eede
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-12 16:17:22 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -436,11 +436,6 @@ stack_priority(){
 
 # WGCF 安装
 install(){
-	# 在已运行 Linux Client 前提下，对于 IPv4 only 只能添加 IPv6 单栈，对于原生双栈不能安装，IPv6 因不能安装 Linux Client 而不用作限制
-	[[ $CLIENT = 3 && $IPV4$IPV6 = 10 && $MODIFY = MODIFYD10 ]] && reading " $T109 " CHANGE &&
-	[[ $CHANGE != [Yy] ]] && exit 1 || MODIFY=MODIFYS10
-	[[ $CLIENT = 3 && $IPV4$IPV6 = 11 ]] && red " $T110 " && exit 1
-	
 	INPUT_LICENSE=1 && input_license
 
 	# OpenVZ / LXC 选择 Wireguard-GO 或者 BoringTun 方案，并重新定义相应的 UP 和 DOWN 指令
@@ -795,10 +790,17 @@ case "$OPTION" in
 1 )	[[ $PLAN = 2 ]] && reading " $T79 " DUAL &&
 	[[ $DUAL != [Yy] ]] && exit 1 || MODIFY=$(eval echo \$MODIFYD$IPV4$IPV6)
 	[[ $PLAN = 1 ]] && MODIFY=$(eval echo \$MODIFYS$IPV4$IPV6)
- 	[[ $PLAN = 3 ]] && yellow " $T80 " && echo $DOWN | sh >/dev/null 2>&1 && exit 1
+ 	[[ $CLIENT != 3 && $TRACE4 = plus || $TRACE4 = on || $TRACE6 = plus || $TRACE6 = on ]] && yellow " $T80 " && echo $DOWN | sh >/dev/null 2>&1 && exit 1
 	install;;
 2 )	[[ $PLAN = 3 ]] && yellow " $T80 " && echo $DOWN | sh >/dev/null 2>&1 && exit 1
 	MODIFY=$(eval echo \$MODIFYD$IPV4$IPV6);	install;;
+	
+	# 在已运行 Linux Client 前提下，对于 IPv4 only 只能添加 IPv6 单栈，对于原生双栈不能安装，IPv6 因不能安装 Linux Client 而不用作限制
+	[[ $CLIENT = 3 && $IPV4$IPV6 = 10 ]] && reading " $T109 " CHANGE &&
+	[[ $CHANGE != [Yy] ]] && exit 1 || MODIFY=MODIFYS10
+	[[ $CLIENT = 3 && $IPV4$IPV6 = 11 ]] && red " $T110 " && exit 1
+	
+	
 [Cc] )	[[ $CLIENT = 3 ]] && red " $T92 " && exit 1 || proxy;;
 [Dd] )	update;;
 * )	menu$PLAN;;
