661ddbc40ec536e19394b6148609b44b75b13e4a
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-14 05:59:13 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -278,10 +278,11 @@ proxy_onoff(){
     PROXY=$(warp-cli --accept-tos status 2>/dev/null)
     [[ -z $PROXY ]] && red " $T93 "
     [[ $PROXY =~ Connected ]] && warp-cli --accept-tos disconnect >/dev/null 2>&1 && warp-cli --accept-tos disable-always-on >/dev/null 2>&1 && 
-    [[ ! $(ss -nltp) =~ 'warp-svc' ]] && green " $T91 "
+    [[ ! $(ss -nltp) =~ 'warp-svc' ]] && green " $T91 "  && break
     [[ $PROXY =~ Disconnected ]] && warp-cli --accept-tos connect >/dev/null 2>&1 && warp-cli --accept-tos enable-always-on >/dev/null 2>&1 && STATUS=1
-    [[ $STATUS = 1 ]] && [[ $(ss -nltp) =~ 'warp-svc' ]] && green " $T90 "
+    [[ $STATUS = 1 ]] && [[ $(ss -nltp) =~ 'warp-svc' ]] && green " $T90 " && break
     [[ $STATUS = 1 ]] && [[ $(warp-cli --accept-tos status 2>/dev/null) =~ Connecting ]] && red " $T96 " && exit 1
+    
     }
 
 # 设置部分后缀 2/3
