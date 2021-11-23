8428c7265288c8d3e01aa12b95899936c88c56b1
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-23 22:14:39 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -361,7 +361,6 @@ CMD=(
 for i in "${CMD[@]}"; do
 	SYS="$i" && [[ -n $SYS ]] && break
 done
-[[ -z $SYSTEM ]] && red " $T5 " && exit 1
 
 REGEX=("debian" "ubuntu" "centos|kernel|'oracle linux'|alma|rocky" "'amazon linux'")
 RELEASE=("debian" "ubuntu" "centos" "centos")
@@ -370,6 +369,7 @@ COMPANY=("" "" "" "amazon")
 for ((i=0; i<${#REGEX[@]}; i++)); do
 	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[i]} ]] && SYSTEM="${RELEASE[i]}" && COMPANY="${COMPANY[i]}" && [[ -n $SYSTEM ]] && break
 done
+[[ -z $SYSTEM ]] && red " $T5 " && exit 1
 
 [[ $LANGUAGE != 2 ]] && T26="Curren operating system is $SYS.\n The script supports Debian 10/11; Ubuntu 18.04/20.24 or CentOS 7/8 only. Feedback: [https://github.com/fscarmen/warp/issues]" || T26="当前操作是 $SYS\n 本脚本只支持 Debian 10/11; Ubuntu 18.04/20.24 和 CentOS 7/8 系统,问题反馈:[https://github.com/fscarmen/warp/issues]"
 [[ $SYSTEM = debian ]] && [[ $(echo "$SYS" | sed 's/[^0-9.]//g' | cut -d . -f1) -lt 10 ]] && red " $T26 " && exit 1
