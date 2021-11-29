c989ffd11b884eb740238f7750d2a69edf5acfc8
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-30 00:26:11 +0800

    Significantly optimize the time for searching MTU

--- menu.sh
+++ menu.sh
@@ -583,20 +583,23 @@ install(){
 	yellow " $T81 "
 	MTU=$((1500-28))
 	[[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.192.1 >/dev/null 2>&1
-	until [[ $? = 0 || MTU -le $((1280+80-28)) ]]
+	until [[ $? = 0 || $MTU -le $((1280+80-28)) ]]
 	do
 	MTU=$((MTU-10))
 	[[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.192.1 >/dev/null 2>&1
 	done
-	[[ $MTU -lt $((1500-28)) ]] && MTU=$((MTU+9))
-	[[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.192.1 >/dev/null 2>&1
-	until [[ $? = 0 || MTU -le $((1280+80-28)) ]]
-	do
-        MTU=$((MTU-1))
-        [[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.192.1 >/dev/null 2>&1      
-	done
+	
+	if [[ $MTU -eq $((1500-28)) ]]; then MTU=$MTU
+	elif [[ $MTU -le $((1280+80-28)) ]]; then MTU=$((1280+80-28))
+	else
+		for ((i=0; i<9; i++)); do
+		(( MTU++ ))
+		( [[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.192.1 >/dev/null 2>&1 ) || break
+		done
+		(( MTU-- ))
+	fi
+	
 	MTU=$((MTU+28-80))
-	[[ $MTU -lt 1280 ]] && MTU=1280
 
 	# 修改配置文件
 	sed -i "s/MTU.*/MTU = $MTU/g" wgcf-profile.conf
