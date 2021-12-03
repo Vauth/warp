e55f18e73a1de1e94a7c799dd77af00368bd80d9
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-03 12:53:43 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -190,24 +190,29 @@ plus(){
 
 # 更换支持 Netflix WARP IP 改编自 [luoxue-bot] 的成熟作品，地址[https://github.com/luoxue-bot/warp_auto_change_ip]
 change_ip(){
+	match(){
+	ip4_info
+	green " Region: ${region} Done, monitoring...\n IP: $WAN4 "
+	}
+	
 	region_area(){
 	region=$(curl --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-]\{1,\}\).*/\1/g')
 	region=${region:-US}
 
 	area2=("${area}" "*")
-	display2=("Region: ${region} Done, monitoring..." "Not match, Changing IP...")
-	cmd2=("" "systemctl restart wg-quick@wgcf")
+	display2=("" "Not match, Changing IP...")
+	cmd2=("match" "systemctl restart wg-quick@wgcf")
 	sleep_sec2=("6" "3")
         
 	for ((j=0; j<${#area2[@]}; j++)); do
 		[[ "$region" == ${area2[j]} ]] && break
 	done
-	echo -e ${display2[j]} && (${cmd2[j]}; sleep ${sleep_sec2[j]})
+	red " ${display2[j]}\c "; ${cmd2[j]}; sleep ${sleep_sec2[j]})
 	}
 
-	output=("404" "403" "000" "200")
-	display1=("Originals Only, Changing IP..." "No, Changing IP..." "Failed, retrying..." "Matching the region...")
-	cmd1=("systemctl restart wg-quick@wgcf" "systemctl restart wg-quick@wgcf" "systemctl restart wg-quick@wgcf" "region_area")
+	output=("404" "403" "200" "000")
+	display1=("Originals Only, Changing IP..." "No, Changing IP..." "" "Failed, retrying...")
+	cmd1=("systemctl restart wg-quick@wgcf" "systemctl restart wg-quick@wgcf" "region_area" "systemctl restart wg-quick@wgcf")
 	sleep_sec1=("3" "3" "0" "0")
 
 	UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
@@ -219,7 +224,7 @@ change_ip(){
 			for ((i=0; i<${#output[@]}; i++)); do
 				[[ "$result" == ${output[i]} ]] && break
 			done
-		echo -e ${display1[i]} && (${cmd1[i]}; sleep ${sleep_sec1[i]})
+		red " ${display1[i]}\c "; ${cmd1[i]}; sleep ${sleep_sec1[i]})
 	done
 	}
 
