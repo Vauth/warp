c47d6ecf0e781044228099d3701f65f8b480f5e9
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-03 22:13:13 +0800

    Update t.sh

--- old/t.sh
+++ old/t.sh
@@ -215,6 +215,7 @@ change_ip(){
 	REGION=$(tr [:lower:] [:upper:] <<< $(curl --user-agent "${UA_Browser}" -$NF -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-]\{1,\}\).*/\1/g'))
 	[[ $RESULT = 200 ]] && REGION=${REGION:-US}
 	ip${NF}_info
+	[[ $LANGUAGE = 2 ]] && COUNTRY=$(expr $(curl -sm4 "http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=$(eval echo \$COUNTRY$NF)") : '.*tgt\":\"\([^"]*\).*')
 	[[ $LANGUAGE != 2 ]] && T125="Region: $REGION Done. IPv$NF: $(eval echo \$WAN$NF)  $(eval echo \$COUNTRY$NF)  $(eval echo \$ASNORG$NF).Retest after 60 seconds."  || T125="$REGION 区域解锁成功，IPv$NF: $(eval echo \$WAN$NF)  $(eval echo \$COUNTRY$NF)  $(eval echo \$ASNORG$NF)，60秒后重新测试"
 	[[ $LANGUAGE != 2 ]] && T126="Try $i. IPv$NF: $(eval echo \$WAN$NF)  $(eval echo \$COUNTRY$NF)  $(eval echo \$ASNORG$NF)"  || T126="尝试第$i次，解锁失败，IPv$NF: $(eval echo \$WAN$NF)  $(eval echo \$COUNTRY$NF)  $(eval echo \$ASNORG$NF)"
 	[[ -z $REGION ]] && red " $T126 " && systemctl restart wg-quick@wgcf || (green " $T125 " && sleep 60)
