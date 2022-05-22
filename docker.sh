92e47e70d8fffa7ffaefd1ece8cd0e5cf7d8a5f4
fscarmen <fscarmen@gmail.com>
2022-05-22 09:29:17 +0000

    Sync WGCF to V2.2.14 by Github Actions, 2022/05/22 09:29:17

--- docker.sh
+++ docker.sh
@@ -606,10 +606,10 @@ install(){
 	}&
 	
 	# 注册 WARP 账户 (将生成 wgcf-account.toml 文件保存账户信息)
-	# 判断 wgcf 的最新版本,如因 github 接口问题未能获取，默认 v2.2.13
+	# 判断 wgcf 的最新版本,如因 github 接口问题未能获取，默认 v2.2.14
 	{	
 	latest=$(wget --no-check-certificate -qO- -T1 -t1 $CDN "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | cut -d : -f2 | sed 's/[ \"v,]//g')
-	latest=${latest:-'2.2.13'}
+	latest=${latest:-'2.2.14'}
 
 	# 安装 wgcf，尽量下载官方的最新版本，如官方 wgcf 下载不成功，将使用 jsDelivr 的 CDN，以更好的支持双栈。并添加执行权限
 	wget --no-check-certificate -T1 -t1 $CDN -O /usr/local/bin/wgcf https://github.com/ViRb3/wgcf/releases/download/v"$latest"/wgcf_"$latest"_linux_$ARCHITECTURE ||
--- pc/mac.sh
+++ pc/mac.sh
@@ -251,10 +251,10 @@ install(){
 	! type -p brew >/dev/null 2>&1 && /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 	! type -p wg >/dev/null 2>&1 && brew install wireguard-tools
 
-	# 判断 wgcf 的最新版本,如因 github 接口问题未能获取，默认 v2.2.13
+	# 判断 wgcf 的最新版本,如因 github 接口问题未能获取，默认 v2.2.14
 	green "\n ${T[${L}11]}\n "
 	latest=$(curl -fsSL "https://api.github.com/repos/ViRb3/wgcf/releases/latest" | grep "tag_name" | head -n 1 | cut -d : -f2 | sed 's/[ \"v,]//g')
-	latest=${latest:-'2.2.13'}
+	latest=${latest:-'2.2.14'}
 	[[ ! -e /usr/local/bin/wgcf ]] && sudo curl -o /usr/local/bin/wgcf https://raw.githubusercontents.com/fscarmen/warp/main/wgcf/wgcf_"$latest"_darwin_"$ARCHITECTURE"
 
 	# 安装 wireguard-go
