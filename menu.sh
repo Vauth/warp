d0bf85b86bfd04720fa6bc6c81146e48549a07c8
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-08-12 11:11:44 +0800

    shows two decimal places for WARP+ traffic

--- menu.sh
+++ menu.sh
@@ -1687,7 +1687,7 @@ check_quota(){
 		API=$(curl -s "https://api.cloudflareclient.com/v0a884/reg/$DEVICE_ID" -H "User-Agent: okhttp/3.12.1" -H "Authorization: Bearer $ACCESS_TOKEN")
 		QUOTA=$(grep -oP '"quota":\K\d+' <<< $API)
 	fi
-	[[ $QUOTA -gt 10000000000000 ]] && QUOTA="$((QUOTA/1000000000000)) TB" ||  QUOTA="$((QUOTA/1000000000)) GB"
+	[[ $QUOTA -gt 10000000000000 ]] && QUOTA="$(echo "scale=2; $QUOTA/1000000000000" | bc) TB" ||  QUOTA="$(echo "scale=2; $QUOTA/1000000000" | bc) GB"
 	}
 
 # 免费 WARP 账户升级 WARP+ 账户
