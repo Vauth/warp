066d5a2fdc55ebb25346f4502d6389ec67726255
fscarmen <fscarmen@gmail.com>
2023-03-02 10:31:00 +0000

    Sync warp-go to V1.0.8 by Github Actions, 2023/03/02 10:31:00

--- warp-go.sh
+++ warp-go.sh
@@ -345,7 +345,7 @@ check_install() {
     {
       # 预下载 warp-go，使用 githubusercontents 的 CDN，以更好的支持双栈。并添加执行权限，如因 gitlab 接口问题未能获取，默认 v1.0.6
       latest=$(wget -qO- -T1 -t1 https://gitlab.com/api/v4/projects/ProjectWARP%2Fwarp-go/releases | awk -F '"' '{for (i=0; i<NF; i++) if ($i=="tag_name") {print $(i+2); exit}}' | sed "s/v//")
-      latest=${latest:-'1.0.6'}
+      latest=${latest:-'1.0.8'}
       wget --no-check-certificate -qO /tmp/warp-go.tar.gz https://raw.githubusercontents.com/fscarmen/warp/main/warp-go/warp-go_"$latest"_linux_"$ARCHITECTURE".tar.gz
       tar xzf /tmp/warp-go.tar.gz -C /tmp/ warp-go
       chmod +x /tmp/warp-go
